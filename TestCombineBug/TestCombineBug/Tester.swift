cd822257110a84d1686d0a41c729dc5b2429c69b
Nikola Milovanovic <nikola.milovanovic@tatatu.com>
2022-12-06 13:35:24 +0100

    removing unused code

--- TestCombineBug/TestCombineBug/Tester.swift
+++ TestCombineBug/TestCombineBug/Tester.swift
@@ -9,10 +9,6 @@ import Foundation
 import Combine
 import SwiftUI
 
-enum SomeError: Error {
-    case some
-}
-
 final class Tester: ObservableObject {
     
     private let subject1 = PassthroughSubject<Int, Never>()
